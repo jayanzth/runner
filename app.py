415cdc4ff169fbe8c4d6249566f6a37ec75b149c
Jayanth Kumar <jayanth.kumar@studentambassadors.com>
2024-10-17 22:31:59 +0530

    New Broke Stuff2

--- app.py
+++ app.py
@@ -56,5 +56,5 @@ def predict():
     return jsonify({"sentiment": prediction[0]})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    port = int(os.environ.get('PORT', 10000))  # Default to 5000 if PORT is not set
     app.run(host='0.0.0.0', port=port)
